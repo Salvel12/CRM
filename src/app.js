86c1be3a4cf0ee42c18e9349fd04efbda9fcca91
Samuel.Alvarez <samuel_alvarez82201@elpoli.edu.co>
2023-09-18 09:37:55 -0500

    primer commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+//config
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
+
+// Configuración de ruta estática para los archivos CSS, JS y otros recursos en la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
