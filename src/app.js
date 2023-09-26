dba3db28904f43c224f39afc26dd202396052a22
Samuel.Alvarez <samuel_alvarez82201@elpoli.edu.co>
2023-09-26 11:07:16 -0500

    refactor: se agregó el modulo ejs para facilitar la creacion del CRUD

--- src/app.js
+++ src/app.js
@@ -4,15 +4,24 @@ const morgan = require('morgan');
 const mysql = require('mysql');
 const myConnection = require('express-myconnection');
 const path = require('path');
-const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
 
 //config
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
 
-
+//middleware
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+  host:localhost,
+  user: root,
+  password: 'contraseña',
+  port: 3306,
+  database:crm
+}, 'single'));
 
 // Configuración de ruta estática para los archivos CSS, JS y otros recursos en la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
