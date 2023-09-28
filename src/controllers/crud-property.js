a32ebe72dd4bbdd8b0fc81e0cb13646f6ebe1574
Zilith <diegozapata063@gmail.com>
2023-09-27 21:49:50 -0500

    Feat
    now you can create a new property

--- src/controllers/crud-property.js
+++ src/controllers/crud-property.js
@@ -1,5 +1,8 @@
 const pool = require('../../database/db')
 
+let client_id = 1234;
+let agent_id;
+
 exports.save = (req, res) => {
     console.log(req.body)
     const Property_ID = req.body.Property_ID;
@@ -9,12 +12,43 @@ exports.save = (req, res) => {
     const time = req.body.time;
     const Name_agent = req.body.Name_agent;
 
-    pool.query('SELECT `customer`.`Csutomer_ID` FROM `customer` WHERE `customer`.`name` = ?',{name: Customer_name}, (error, results) =>{
-        if (error) {
-            throw error;
-        } else {
-            console.log('Resultados 2: ' + results);
-        }
+    const getCustomerID = new Promise((resolve, reject) => {
+        const customer = `SELECT \`customer\`.\`Csutomer_ID\` FROM \`customer\` WHERE \`customer\`.\`name\` = '${Customer_name}'`;
+
+        pool.query(customer, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                client_id = results[0].Csutomer_ID;
+                resolve();
+            }
+        });
+
     })
-    //pool.query('INSERT INTO property SET ?', { address: address, category_ID: category_ID, Customer_ID: })
+
+    const getAgentID = new Promise((resolve, reject) => {
+        const agent = `SELECT \`agent\`.\`Agent_ID\` FROM \`agent\` WHERE \`agent\`.\`Name_agent\` = '${Name_agent}'`;
+
+        pool.query(agent, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                agent_id = results[0].Agent_ID;
+                resolve();
+            }
+        });
+    })
+
+    Promise.all([getCustomerID, getAgentID]).then(() => {
+        console.log("Id del cliente " + client_id);
+        
+        pool.query('INSERT INTO property SET ?', { address: address, category_ID: category_ID, Customer_ID: client_id, time: time, Agent_ID: agent_id }, (error, results) => {
+            if (error)
+                throw error;
+            else
+                res.redirect('/list-properties')
+        });
+    }).catch(error => {
+        throw error;
+    });
 }
